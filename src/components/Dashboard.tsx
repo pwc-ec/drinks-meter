33590d701500e74e627fc61ef8b7a0c3332c1c36
jkycek001 <jonas.kycek@pwc.com>
2017-12-13 15:54:52 +0100

    dashboard layout

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 
+import * as cn from 'classnames'
+
 import { Button, Grid, IconButton, Typography } from 'material-ui'
 import MenuIcon from 'material-ui-icons/Menu'
 import { withStyles } from 'material-ui/styles'
@@ -16,9 +18,20 @@ export interface IDashboardProps {
 }
 
 const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexGrow: 1,
+  borderBottom: {
+    borderBottom: '0.05rem solid rgba(255,255,255,0.2)',
+  },
+  borderRight: {
+    borderRight: '0.05rem solid rgba(255,255,255,0.2)',
+  },
+  button: {
+    marginRight: '0.25rem',
+  },
+  cell: {
+    padding: '1rem',
+  },
+  fullHeight: {
+    height: '100%',
   },
   root: {
     background: theme.bmai.palette.background,
@@ -27,6 +40,9 @@ const styles = theme => ({
     flexDirection: 'column' as 'column',
     height: '100%',
     overflow: 'hidden' as 'hidden',
+  },
+  title: {
+    fontWeight: 200 as 200,
   },
 })
 
@@ -37,23 +53,44 @@ const Dashboard: React.SFC<IDashboardProps> = ({ currentEvent, classes, loading 
     ) : (
       <Grid
         className={classes.container}
+        alignContent="stretch"
         alignItems="center"
         direction="row"
         container={true}
         justify="center"
-        spacing={40}
+        xs={true}
+        spacing={0}
       >
-        <Grid className={classes.center} item={true} xs={6}>
-          <DrinksTotals eventId={currentEvent && currentEvent.id} />
-          <DrinksThroughTime eventId={currentEvent && currentEvent.id} />
+        <Grid className={classes.fullHeight} item={true} xs={true}>
+          <Grid className={classes.fullHeight} container={true} xs={true} spacing={0} direction="column">
+            <Grid className={cn(classes.borderBottom, classes.borderRight, classes.cell)} item={true} xs={true}>
+              <DrinksTotals eventId={currentEvent && currentEvent.id} />
+            </Grid>
+            <Grid className={cn(classes.borderRight, classes.cell)} item={true} xs={true}>
+              <DrinksThroughTime eventId={currentEvent && currentEvent.id} />
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid className={classes.center} item={true} xs={6}>
-          <Link to={`/${currentEvent.url}`}>
-            <IconButton className={classes.button} aria-label="Back">
-              <MenuIcon />
-            </IconButton>
-          </Link>
-          <Typography type="subheading">{currentEvent.name}</Typography>
+        <Grid className={classes.fullHeight} item={true} xs={true}>
+          <Grid className={classes.fullHeight} container={true} xs={true} spacing={0} direction="column">
+            <Grid className={cn(classes.borderBottom, classes.cell)} item={true} xs={true}>
+              <Grid container={true} direction="row" alignItems="center">
+                <Link to={`/${currentEvent.url}`} className={classes.button}>
+                  <IconButton className={classes.button} aria-label="Back">
+                    <MenuIcon />
+                  </IconButton>
+                </Link>
+                <Typography type="title" className={classes.title}>
+                  {currentEvent.name}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid className={classes.cell} item={true} xs={true}>
+              <Typography type="title" className={classes.title}>
+                Party Index
+              </Typography>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     )}
